4ae1af3abc7211149ad87b48a7805888473c3087
sunilkv <sunilkv@bbnl.co.in>
2019-12-30 18:48:14 +0530

    MVP with refractor

--- Mvptut2/app/src/main/java/com/sunil/mvptut2/presenter/MainActivityPresenter.java
+++ Mvptut2/app/src/main/java/com/sunil/mvptut2/presenter/MainActivityPresenter.java
@@ -1,10 +1,19 @@
 package com.sunil.mvptut2.presenter;
 
+import android.view.View;
+
 import com.sunil.mvptut2.contract.MainActivityContract;
+import com.sunil.mvptut2.model.Users;
 
 public class MainActivityPresenter implements MainActivityContract.presenter {
 
+    Users users;
+    View view;
 
+    public MainActivityPresenter(View view) {
+        this.view = view;
+        this.users= new Users();
+    }
 
     @Override
     public void loadMessage() {
@@ -13,11 +22,12 @@ public class MainActivityPresenter implements MainActivityContract.presenter {
 
     @Override
     public void UpdateUserFullName(String firstName) {
-
+    this.users.setFullName(firstName);
     }
 
     @Override
     public void UpdateUserEmail(String email) {
+        this.users.setFullName(email);
 
     }
 }
